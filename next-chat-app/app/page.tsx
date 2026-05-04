dbac0985cf327cc9da1ecadcaa75c58112f8aee8
Mohd-Zeeshan-git <mohdzeeshanhasan06@gmail.com>
2026-05-04 09:51:46 +0000

    feat: add interactive 3D card component and animated tooltip
    - Implemented a 3D card component with mouse hover effects in `3d-card.tsx`.
    - Created an animated tooltip component in `animated-tooltip.tsx` for displaying additional information on hover.
    - Introduced background beams for visual enhancement in `background-beams.tsx`.
    - Added a hover effect component for cards in `card-hover-effect.tsx`.
    - Developed a wavy background effect in `wavy-background.tsx` for dynamic visuals.
    - Created a contact page with a form in `contact/page.tsx` for user inquiries.
    - Implemented a courses page displaying available music courses in `courses/page.tsx`.
    - Updated course images in `music_courses.json` to use a new URL.
    - Modified the main page layout in `page.tsx` to include new components and improve structure.
    - Added `simplex-noise` dependency for noise generation in wavy background.

--- next-chat-app/app/page.tsx
+++ next-chat-app/app/page.tsx
@@ -1,13 +1,11 @@
 
 import FeaturedCourses from "./components/FeaturedCourses";
-// import Footer from "@/components/Footer";
+import Footer from "@/app/components/Footer";
 import HeroSection from "./components/HeroSection";
-// import Instructors from "@/components/Instructors";
+import Instructors from "@/app/components/Instructors";
 import MusicSchoolTestimonials from "./components/TestimonialCards";
-// import UpcomingWebinars from "@/components/UpcomingWebinars";
+import UpcomingWebinars from "./components/UpcomingWebinars";
 import WhyChooseUs from "./components/WhyChooseUs";
-
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -15,9 +13,9 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
-      {/* <UpcomingWebinars />
+      <UpcomingWebinars />
       <Instructors />
-      <Footer /> */}
+      <Footer />
     </main>
   );
 }
